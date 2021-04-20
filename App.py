744977f9f80f2370a31cd2753cc89c0dca79175a
JoeGreaves17 <74414406+JoeGreaves17@users.noreply.github.com>
2021-04-20 20:22:43 +0100

    Day 1 (2)
    Started log form

--- App.py
+++ App.py
@@ -26,7 +26,16 @@ def DownloadLog():
 window = Tk()
 window.title("On/Off Monitor")
 window.resizable(0,0)
+fileagedl = StringVar() #Fileage for download logs
+fileaged = StringVar() #Fileage for delete logs
 Label(window,text="Log files").grid(row=1,column=1)
 Label(window,text="Download").grid(row=2,column=1)
-Button(window,text="Download",command=DownloadLog).grid(row=3,column=1)
+lognumdl = Entry(window)
+lognumdl.grid(row=3,column=1)
+Radiobutton(window,text="of the oldest files",variable=fileagedl,value="old").grid(row=4,column=1)
+Radiobutton(window,text="of the newest files",variable=fileagedl,value="new").grid(row=5,column=1)
+Button(window,text="Download",command=DownloadLog).grid(row=6,column=1)
+Label(window,text="Delete").grid(row=7,column=1)
+lognumd = Entry(window)
+lognumd.grid(row=8,column=1)
 window.mainloop()
